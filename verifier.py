7764d8de217c40bc6dbf13718c9b79f1cbd34789
George Almasi <gheorghe@us.ibm.com>
2022-02-24 18:32:41 +0000

    Verification works

--- verifier.py
+++ verifier.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
 from uefi_firmware.uefi import *
-import hashlib
-#from OpenSSL import crypto
-import M2Crypto
-import pyasn1.codec.der.decoder as asn1dec
-from pyasn1_modules import rfc2315
-import pyasn1
 import base64
-
-from Crypto.PublicKey import RSA
-
-#import pprint
-import texttable as tt
+import hashlib
+import M2Crypto
+import pyasn1
+import pyasn1.codec.der.decoder as asn1dec
+from pyasn1_modules import rfc2315, rfc4055
 
 EFI_FFS_FILE_HEADER_SIZE=24
 
@@ -20,71 +14,84 @@ def check_firmware_sig(fname):
     fp = open(fname, 'rb')
     data = fp.read()
 
+    # ###################################################
     # process all firmware volumes
+    # ###################################################
+
     volume_indices = search_firmware_volumes (data)
     firmware_volume_list=[]
     for index in volume_indices:
-        print("volume at index 0x%08x 0x%08x"%(index, index-40))
+        print("Found firmware volume at index 0x%08x 0x%08x"%(index, index-40))
         datastart = data[index-40:]
         name = index-40
         firmware_volume = FirmwareVolume(datastart, name)
         firmware_volume.process()
         firmware_volume_list.append(firmware_volume)
 
-    # find the firmware file marked by a special GUID: we are looking for the signature table.
-    guid='414D94AD-998D-47D2-BFCD-4E882241DE32'
-    offset, length = find_guid_offset (firmware_volume_list, guid)
-    if not offset: exit(1)
-    print('--> sigtable guid %s found offset=0x%x size=%d'%(guid, offset, length))
+    # ###################################################
+    # find the firmware file marked by a special GUID:
+    # we are looking for the signature table.
+    # ###################################################
 
-    # extract and dump the signature table
-    table = extract_sig_table (data, offset, length)
-    tab = tt.Texttable()
-    tab.header(('Address', 'length', 'signed'))
-    for entry in table:
-        tab.add_row(("0x%08x"%(entry[0]), entry[1], entry[2]))
-    print("Signature table")
-    print("===============")
-    print(tab.draw())
+    sig_guid='414D94AD-998D-47D2-BFCD-4E882241DE32'
+    sig_offset, sig_length = find_guid_offset (firmware_volume_list, sig_guid)
+    if not sig_offset: exit(1)
+    print('+-------------------------------------------+-------------------+-----------------+')
+    print('| guid %36s | offset=0x%08x | size=%10d |'%(sig_guid, sig_offset, sig_length))
+    print('+-------------------------------------------+-------------------+-----------------+')
     print()
 
+    # ###################################################
+    # extract and dump the signature table
+    # ###################################################
+
+    table = extract_sig_table (data, sig_offset, sig_length)
+    print("+------------------------------+")
+    print("|     Signature table          |")
+    print("+----------+----------+--------+")
+    print("| address  |  length  | signed |")
+    print("+----------+----------+--------+")
+    for entry in table:
+        print("|0x%08x|%10d|%8d|"%(entry[0], entry[1], entry[2]))
+    print("+----------+----------+--------+")
+    print()
+
+    # ###################################################
     # collect the digest of all signed regions.
+    # ###################################################
+
     digest=collect_signeddata (data, table)
-    print("============")
-    print("| Digest   |")
-    print("============")
-    print("%s"%(digest))
+    print("+--------+" + '-'*64 + '+')
+    print("| Digest |%64s|"%(digest.hex()))
+    print("+--------+" + '-'*64 + '+')
     print()
     
+    # ###################################################
+    # extract the signed digest
+    # ###################################################
+
+    sigbody=extract_sig(data, sig_offset, sig_length)
+    print('+-------------------+')
+    print('| Digital Signature |')
+    print('+-------------------+')
+    print(sigbody.hex())
+    print()
+
     # extract the public key
     pkcert_guid='3A666558-43E3-4D25-9169-DB81F5DB42E1'
     pkcert_offset, pkcert_length = find_guid_offset(firmware_volume_list, pkcert_guid)
     if not pkcert_offset: exit(1)
-    print('--> pkcert guid %s found offset=0x%x size=%d'%(pkcert_guid, pkcert_offset, pkcert_length))
-    pkcert, rsakey = m2crypto_extract_cert_pubkey (data, pkcert_offset, pkcert_length)
-
+    print('+-------------------------------------------+-------------------+-----------------+')
+    print('| guid %36s | offset=0x%08x | size=%10d |'%(pkcert_guid, pkcert_offset, pkcert_length))
+    print('+-------------------------------------------+-------------------+-----------------+')
+    pkcert, rsakey = extract_cert_pubkey (data, pkcert_offset, pkcert_length)
     print("--> pkcert extracted, signer=%s"%(pkcert.get_issuer()))
-
-    # extract the signed digest
-    print("\nDigital Signature")
-    print("===================")
-    cert=extract_signed_digest(data, offset, length)
-    print(cert)
-#    print("--> pkcs7 extracted, is_signed = %d is_data=%d"%(signedkey.type_is_signed(), signedkey.type_is_data()))
-
-    # load the certificate
-    #s = M2Crypto.SMIME.SMIME()
-    #sk = M2Crypto.X509.X509_Stack()
-    #sk.push(pkcert)
-    #s.set_x509_stack(sk)
-
-    # load the pkcs7 data
-    #p7, data
-    
-#    pkcs7 = M2Crypto.SMIME.PKCS7(signedkey, 1)
-#    pkcs7.get0_signers(M2Crypto.X509.X509_Stack())
     
 
+    vstatus=rsakey.verify(digest, sigbody, 'sha256')
+    print('+------------------------+')
+    print('| Verification status: %1d |'%(vstatus))
+    print('+------------------------+')
     return
         
 # #########################################################
@@ -127,13 +134,12 @@ def extract_sig_table (data, offset, length):
 # extract the signed digest of a firmware file
 # #########################################################
 
-def extract_signed_digest(data, offset, length):
+def extract_sig(data, offset, length):
     # skip over 116 bytes (?)
     offset+=116
 
     # check that the capsule GUID is what we want it to be.
     # then skip past the capsule header + 4 bytes of padding
-    print(unpack_guid(data[offset:]))
     offset+=32
 
     # WIN_CERTIFICATE header
@@ -151,37 +157,37 @@ def extract_signed_digest(data, offset, length):
     offset += 8+16
 
     # start parsing the certificate
-    seq, remain = asn1dec.decode(data[offset:offset+wc_dwlen-24-fudge], asn1Spec=rfc2315.ContentInfo())
-    print(seq)
-    # the envelope is a sequence
-#    if type(seq) != pyasn1.type.univ.SequenceOf:
-#        exit(1)
-#    print(seq.componentType)
-#    print(seq.getComponentByPosition(0))
-#    c = asn1dec.decode(c)
-#    if type(t) != pyasn1.type.univ.SequenceOf: exit(1)
-#    t,c = asn1dec.decode(c)    
-    
-    exit(0)
-    
-    #asn1crypto.Certificate.load(data[offset:offset+wc_dwlen-24-fudge])
-    
-    #cert=crypto.load_pkcs7_data(crypto.FILETYPE_ASN1, data[offset:offset+wc_dwlen-24-fudge])
-    #return cert
-    
-    #p7, bio=M2Crypto.SMIME.smime_load_pkcs7_bio(M2Crypto.BIO.MemoryBuffer(data[offset:offset+wc_dwlen-24-fudge]))
-    #return p7, bio
+    substrate=data[offset:offset+wc_dwlen-24-fudge]
 
-def m2crypto_extract_cert_pubkey (data, offset, length):
+    # the first component is the pkcs7-data marker.
+    b1, remain1 = asn1dec.decode(substrate, asn1Spec=rfc2315.ContentInfo())
+    contenttype = b1.getComponentByName('contentType')
+    assert contenttype == (1, 2, 840, 113549, 1, 7, 1)
+
+    # the second component is a sequence with signature metadata
+    b2, remain2 = asn1dec.decode(remain1)
+    assert type(b2) == pyasn1.type.univ.Sequence
+    
+    # the third component is a set containing a sequence
+    # the 5th component of the sequence is the 256 byte RSA encrypted sig.
+    b3, remain3 = asn1dec.decode(remain2)
+    assert type(b3) == pyasn1.type.univ.SetOf
+    b31 = b3.getComponentByPosition(0)
+    assert type(b31) == pyasn1.type.univ.Sequence
+    b314 = b31.getComponentByPosition(4)
+    assert type(b314) == pyasn1.type.univ.OctetString
+    assert len(b314) == 256
+    return bytes(b314)
+
+# #########################################################
+# #########################################################
+
+def extract_cert_pubkey (data, offset, length):
     offset += 100
     pkcert=M2Crypto.X509.load_cert_string(data[offset:offset+length], M2Crypto.X509.FORMAT_DER)
     pubkey=pkcert.get_pubkey()
     rsakey=pubkey.get_rsa()
     return (pkcert, rsakey)
-
-
-def crypto_extract_cert (data, offset, length):
-    True
 
 
 # #########################################################
@@ -204,14 +210,10 @@ def collect_signeddata (data, table):
             signedlen += length
         else:
             unsignedlen += length
-    fp=open('body.bin', 'wb')
-    fp.write(body)
-    fp.close()
-    return m.hexdigest()
-    
+    return m.digest()
 
-
-
+# #########################################################
+# #########################################################
 
 def unpack_guid (data):
     w1=struct.unpack('<LHH',data[:8])
@@ -221,7 +223,8 @@ def unpack_guid (data):
 
 
 
-
+# #########################################################
+# #########################################################
 
 check_firmware_sig('/home/galmasi/code/firmware/supermicro/IBM_X11QPH_20211018_storage.ROM')
 #check_firmware_sig('/home/galmasi/repo/cloud-infrastructure/xCAT_Scripts/install/firmware/smc/Softlayer_X11QPH_20200805.ROM_Signed')
